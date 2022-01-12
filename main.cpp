bdecb07c1a43efb0e6e194c1a74e9785464794d4
Lavansurendra <lavansurendra@gmail.com>
2022-01-11 20:42:15 -0500

    Outro

--- main.cpp
+++ main.cpp
@@ -22,23 +22,125 @@ void Rules()
   // function to read out all of the rules
   cout << endl;
   cout << "\033[47;30m Here's how to play!" << endl;
-  usleep(1050000);
+  usleep(1500000);
   cout << "To start, select a difficulty level." << endl;
-  usleep(1050000);
+  usleep(2350000);
   cout << "Easy level starts with $50 000 and 60 percent emmisions" << endl;
-  usleep(1050000);
+  usleep(4000000);
   cout << "Medium level starts with $25 000 and 70 percent emmisions" << endl;
-  usleep(1050000);
+  usleep(4000000);
   cout << "Hard level starts with $10 000 and 80 percent emmisions" << endl;
-  usleep(1050000);
+  usleep(4000000);
   cout << "Then, you will be asked questions to test your skill at city planning" << endl;
-  usleep(1050000);
-  cout << "Each answer will affect your total emissions and money earned, and if you run out of money or emissions reaches 100 you will lose!" << endl;
-  usleep(1050000);
+  usleep(4000000);
+  cout << "Each answer will affect your total emissions and money earned, and if you run out of money or emissions reaches 100% you will lose!" << endl;
+  usleep(8000000);
   cout << "Without further ado, lets begin!\033[0m" << endl;
-  usleep(1050000);
+  usleep(3500000);
   cout << endl;
 }
+
+void rollCredits(){
+  cout << "This project was created to test the limits of your abilities" << endl;
+  usleep(3150000);
+  cout << "The task of taking control of the future is quite difficult  " << endl;
+  usleep(4000000);
+  cout << "No matter how you did, I'm proud of you for taking that      " << endl;
+  usleep(3500000);
+  cout << "Tremendous Resposibility...                                  " << endl;
+  usleep(3000000);
+  cout << "Take some time to reflect on your successes and failures     " << endl;
+  usleep(4000000);
+  cout << "The game keeps track of both money and greenhouse emmisions  " << endl;
+  usleep(4000000);
+  cout << "But it does not keep track of your own opinions and morals   " << endl;
+  usleep(4500000);
+  cout << "Feel free to play again any time :)                          " << endl;
+  usleep(3000000);
+  cout << "                             *                               " << endl;
+  usleep(500000);
+  cout << "                            /|\\                              " << endl;               
+  usleep(500000);
+  cout << "                           /*|O\\                             " << endl;
+  usleep(500000);
+  cout << "                          /*/|\\*\\                            " << endl;
+  usleep(500000);
+  cout << "                         /X/O|*\\X\\                           " << endl;
+  usleep(500000);
+  cout << "                        /*/X/|\\X\\*\\                          " << endl;
+  usleep(500000);
+  cout << "                       /O/*/X|*\\O\\X\\                         " << endl;
+  usleep(500000);
+  cout << "                      /*/O/X/|\\X\\O\\*\\                        " << endl;
+  usleep(500000);
+  cout << "                     /X/O/*/X|O\\X\\*\\O\\                       " << endl;
+  usleep(500000);
+  cout << "                    /O/X/*/O/|\\X\\*\\O\\X\\                      " << endl;
+  usleep(500000);
+  cout << "                            |X|                              " << endl;
+  usleep(500000);
+  cout << "                            |X|                              " << endl;
+  usleep(500000);
+  cout << "                   A Game by Lavan Surendra                  \033[0m" << endl;
+  usleep(3000000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  usleep(500000);
+  cout << endl;
+  system("clear");
+}
+
 int main()
 {
   string running;
@@ -77,11 +179,11 @@ int main()
       break;
     }
     system("clear");
-    usleep(1050000);
+    usleep(1000000);
     cout << "\033[47;30m Current Emmisions: " << emissions << "%" << endl;
-    usleep(1050000);
+    usleep(1000000);
     cout << "Current Cash: $" << cash << endl;
-    usleep(1050000);
+    usleep(1000000);
 
     int aemissions;
     int acost;
@@ -155,6 +257,7 @@ int main()
           if (counter <= 4)
           {
             cout << line << '\n';
+            usleep(3000000);
           }
           else if (counter == 6)
           {
@@ -225,11 +328,11 @@ int main()
         {
           break;
         }
-        usleep(1050000);
+        usleep(1000000);
         cout << "\033[47;30m Current Emmisions: " << emissions << "%" << endl;
-        usleep(1050000);
+        usleep(1000000);
         cout << "Current Cash: $" << cash << endl;
-        usleep(1050000);
+        usleep(1000000);
         currentfile.close();
        
       }
@@ -237,12 +340,16 @@ int main()
     }
     if(round == 20){
       cout << "Congratulations, you defeated all of this game's challenges while keeping emissions down and still having money leftover." << endl;
-      usleep(1050000);
+      usleep(4000000);
       cout << "Thanks for playing" <<endl;
+      system("clear");
+      rollCredits();
     }else{
       cout << "You failed to save the city." <<endl;
-      usleep(1050000);
+      usleep(4000000);
       cout << "Thanks for playing" <<endl;
+      system("clear");
+      rollCredits();
     }
 
  
