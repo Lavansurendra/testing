6302aefb49c1d28c975f2b96e07fd1da40a8ae6c
Lavansurendra <lavansurendra@gmail.com>
2022-01-12 11:07:14 -0500

    added error conditions and some comments

--- main.cpp
+++ main.cpp
@@ -19,17 +19,17 @@ string generateRandomQuestion()
 }
 void Rules()
 {
-  // function to read out all of the rules
+  // function to read out all of the rules, spaced out by pauses to prevent users from being overwhelmed by a block of text
   cout << endl;
   cout << "\033[47;30m Here's how to play!" << endl;
   usleep(1500000);
   cout << "To start, select a difficulty level." << endl;
   usleep(2350000);
-  cout << "Easy level starts with $50 000 and 60 percent emmisions" << endl;
+  cout << "Easy level starts with $100 000 and 60 percent emmisions" << endl;
   usleep(4000000);
-  cout << "Medium level starts with $25 000 and 70 percent emmisions" << endl;
+  cout << "Medium level starts with $80 000 and 70 percent emmisions" << endl;
   usleep(4000000);
-  cout << "Hard level starts with $10 000 and 80 percent emmisions" << endl;
+  cout << "Hard level starts with $50 000 and 80 percent emmisions" << endl;
   usleep(4000000);
   cout << "Then, you will be asked questions to test your skill at city planning" << endl;
   usleep(4000000);
@@ -41,6 +41,7 @@ void Rules()
 }
 
 void rollCredits(){
+  // Function which prints out a thank you and reflection piece at the end of the playthrough, along with a text based tree based off of our early lessons.
   cout << "This project was created to test the limits of your abilities" << endl;
   usleep(3150000);
   cout << "The task of taking control of the future is quite difficult  " << endl;
@@ -143,41 +144,48 @@ void rollCredits(){
 
 int main()
 {
+  //Variables used to take input of whether the user would like to play, or hear the rules
   string running;
   string rules;
+  //Variables to keep track of game ending conditions and the selected user difficulty
   int cash;
   int emissions;
   int difficulty;
+  //Starting the game if the user is ready 
   cout << "\033[47;30m Good Day! Welcome to the Urban City Planner!\n In this game you will act as a City Planner and be faced with many situations and dillemas which you must solve!\n Are you ready for the responsibility of a whole city? [yes/no]\033[0m" << endl;
   cin >> running;
   if ((running == "yes") || (running == "Yes"))
   {
+    // Asking the players if they want to read the rules and calling the Rules function if they do 
     cout << "\033[47;30m Would you like to hear the rules? [yes/no]\033[0m" << endl;
     cin >> rules;
     if ((rules == "yes") || (rules == "Yes"))
     {
       Rules();
     }
+    //Having the user select a difficulty level and adjusting the cash and emmisions values accordingly 
     cout << "\033[47;30m What difficulty level would you like to start with? Easy = 1, Medium = 2, Hard = 3\033[0m" << endl;
     cin >> difficulty;
     switch (difficulty)
     {
     case 1:
-      cash = 50000;
+      cash = 100000;
       emissions = 60;
-      cout << "Starting Cash value: $200 000, Emissions 10%" << endl;
+      cout << "Starting Cash value: $100 000, Emissions 60%" << endl;
       break;
     case 2:
-      cash = 25000;
+      cash = 80000;
       emissions = 70;
-      cout << "Starting Cash value: $100 000, Emissions 20%" << endl;
+      cout << "Starting Cash value: $85 000, Emissions 70%" << endl;
       break;
     case 3:
-      cash = 10000;
+      cash = 50000;
       emissions = 80;
-      cout << "Starting Cash value: $50 000, Emissions 30%" << endl;
+      cout << "Starting Cash value: $50 000, Emissions 80%" << endl;
       break;
     }
+    //printing out current emmissions and cash
+    usleep(1000000);
     system("clear");
     usleep(1000000);
     cout << "\033[47;30m Current Emmisions: " << emissions << "%" << endl;
@@ -185,6 +193,7 @@ int main()
     cout << "Current Cash: $" << cash << endl;
     usleep(1000000);
 
+    //Variables to store the change in emmisions of each option for every question
     int aemissions;
     int acost;
     int bemissions;
@@ -193,9 +202,13 @@ int main()
     int ccost;
     int demissions;
     int dcost;
+    //Variable to keep track of how many rounds have completed
     int round = 0;
+    // Boolean used to keep track of if a question hasn't been found yet
     bool questionNotFound = true;
+    // String to keep track of the file name selected by the random function
     string filename;
+    // List which keeps track of all the unopened files and has elements which are removed when files are found
     string unopenedFiles[20] = {
         "1.txt",
         "2.txt",
@@ -218,11 +231,10 @@ int main()
         "19.txt",
         "20.txt",
     };
-    while ((emissions < 100) && (cash > 0))
+    while ((emissions < 100) && (cash > 0) && (round!=20))
     {
 
       questionNotFound = true;
-      round++;
       cout << "Round #" << round <<endl;
       aemissions = 0;
       acost = 0;
@@ -296,6 +308,10 @@ int main()
         char choice;
         cout << "What will you do?\033[0m" << endl;
         cin >> choice;
+        while((choice!='a') && (choice!='b') &&(choice!='c') && (choice!='d')){
+          cout << "Invalid! please select a valid option\033[0m" << endl;
+          cin >> choice;
+        }
         switch (choice)
         {
         case 'a':
@@ -324,10 +340,7 @@ int main()
         {
           cash = 0;
         }
-         if (round == 20)
-        {
-          break;
-        }
+        round++;
         usleep(1000000);
         cout << "\033[47;30m Current Emmisions: " << emissions << "%" << endl;
         usleep(1000000);
